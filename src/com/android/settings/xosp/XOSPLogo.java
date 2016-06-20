b17d847f07b342c079dc8a20991a2dcc4ffb81fb
Calin Neamtu <nilac8991@gmail.com>
2016-06-20 13:06:24 +0200

    Update XOSP Banner in About Phone
    Animate that sh*t!

---
+++ src/com/android/settings/xosp/XOSPLogo.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2016 Xperia Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.xosp;
+
+import android.os.Bundle;
+import android.graphics.drawable.AnimationDrawable;
+import android.widget.ImageView;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+
+import com.android.settings.R;
+
+public class XOSPLogo extends Preference {
+
+    AnimationDrawable XOSPAnimation;
+
+    @Override
+    protected void onBindView(View rootView) {
+        super.onBindView(rootView);
+
+        ImageView XOSPImage = (ImageView) findViewById(R.id.xosp_logo);
+        XOSPImage.setBackgroundResource(R.drawable.xosp_logo);
+        XOSPAnimation = (AnimationDrawable) XOSPImage.getBackground();
+        XOSPAnimation.start();
+    }
+}
